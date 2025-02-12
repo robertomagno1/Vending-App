c16d59534d273d64ee69b3c05698beeefc057486
Roberto Magno Mazzotta <150812681+robertomagno1@users.noreply.github.com>
2025-02-13 00:24:05 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,29 @@
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+// Configurazione del database
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Avvio del server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
